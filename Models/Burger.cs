ec8429fb8f087ab3cece679603f19bbb9e7af43b
Amy Cherrez <amy.cherrez@udla.edu.ec>
2023-10-11 08:15:26 -0500

    Agregar archivos de proyecto.

---
+++ Models/Burger.cs
@@ -0,0 +1,9 @@
+﻿namespace AmyCherrez_EjercicioCF.Models
+{
+    public class Burger
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmyCherrez_EjercicioCF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
