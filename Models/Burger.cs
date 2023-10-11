ebe44d4b7d459de2026719626ac79e116b4403ab
Amy Cherrez <amy.cherrez@udla.edu.ec>
2023-10-11 08:39:48 -0500

    Creacion de la primera tabla

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,9 +1,16 @@
-﻿namespace AmyCherrez_EjercicioCF.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmyCherrez_EjercicioCF.Models
 {
     public class Burger
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
 
+        [Required]
         public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+
+        [Range(0.01,99.99)]
+        public decimal Precio { get; set; }
     }
 }
